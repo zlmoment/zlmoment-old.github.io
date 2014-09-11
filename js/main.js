bd6864ca6680f24500ed09d20c822d9395e4da42
Zhaoyu Li <zlmoment@gmail.com>
2014-09-11 15:23:07 -0500

    update blog theme

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
   $('a.blog-button').click(function() {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
-    $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
-    $('.content-wrapper').addClass('animated slideInLeft');
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.content-wrapper').addClass('animated slideInRight');
+    } else {
+      $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'max-width': '430px', 'width': '32%'}, 400, swing = 'swing', function() {} );
+    }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
